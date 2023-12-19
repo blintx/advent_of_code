e63f33734c98ac7bf72a551902351771abfce301
Blintx40 <balint.retvari@gmail.com>
2023-12-19 19:38:19 +0100

    dec05

--- dec.05.py
+++ dec.05.py
@@ -1,29 +1,55 @@
 data = []
+seeds = []
+seed_to_soil =[]
+soil_to_fertilizer= []
+fertilizer_to_water= []
+water_to_light = []
+light_to_temperature = []
+temperature_to_humidity = []
+humidity_to_location = []
 
-with open('data/input5.txt','r',encoding='utf-8')as f :
+with open('data/input5.txt','r',encoding='utf_8')as f :
     for i in f.readlines():
         data.append(i.strip())
         
+dictionary = {}  
+index = 0
+count= 0
+while index < len(data):
+    if data[index] == '':
+        data[index] = int(count)
+        count += 1
+        
+    index += 1
 
+
+dictionary = {}  
+count = 0
+for i in range(len(data)):
+    # print(str(type(data[i])))
+
+    if ('int' in str(type(data[i]))):
+        print('x')
+        # print(data[i+2:data.index(count+1)])
+        # print(data[i+1])
+        
+        if 0<=count <7:
+            dictionary.update({data[i+1]:data[i+2:data.index(count+1)]})
+            
+            
+        else:
+            dictionary.update({data[i+1]:data[i+2:]})
+        count+=1
+        print(count)
+            
+        
         
 
-dictionary = {}
+
+
+
+data_2 = []
+print(dictionary)
+
 
 dictionary.update({data[0].split(':')[0]:data[0].split(':')[1].replace(' ','',1).replace(' ',';').split(';')})
-
-
-for i in range(len(data)):
-    if data[i] =='':
-        data[i+1] = data[i+1].replace(':','')
-        dictionary.update({data[i+1]:[]})
-        j = 0
-        while data[i+j] != '':
-            
-            dictionary[data[i+1]].append(data[i+j])
-            if i+j >= len(data):
-                break
-            else:
-                j+=1
-            
-        
-print(dictionary)      
---
+++ test.py
@@ -0,0 +1,4 @@
+lista = ['0','valami','valak','val','1']
+print(lista[lista.index('0')+1:lista.index('1')])
+if str(type(0)=='int'):
+    print('yessir')
